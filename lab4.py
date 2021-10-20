30ff1f0a200c8691babfbcfcf25585428e4d5a52
andrabotizan <andra_06_08@yahoo.com>
2021-10-20 12:43:43 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,21 +1,21 @@
-def read(list1, list2):
-    n = int(input("Dimensiune lista1: "))
+def read(list):
+    n = int(input("Dimensiune lista: "))
     for i in range(0, n, 1):
-        list1.append(int(input("Itroduceti un element: ")))
-    n = int(input("Dimensiune lista2: "))
-    for i in range(0, n, 1):
-        list2.append(int(input("Itroduceti un element: ")))
-def numara_elementele_pare(list)
+        list.append(int(input("Itroduceti un element: ")))
+
+
+def numara_elementele_pare(list):
     k = 0
-    for i in range(0,len(list)+1,1)
-        if list[i] % 2 == 0
-            k ++
+    for i in range(0,len(list),1):
+        if list[i] % 2 == 0:
+            k = k + 1
     return k
+
 def intersectia_listelor(list1,list2):
     list_intersectie = []
-    for i in range(0,len(list1)+1,1)
-        for j in range(0,len(list2)+1,1)
-            if list1[i] == list2[j]
+    for i in range(0,len(list1),1):
+        for j in range(0,len(list2),1):
+            if list1[i] == list2[j]:
                 list_intersectie.append(list1[i])
     return list_intersectie
 def concatenare_elemente_palindrom(list1,list2):
@@ -30,24 +30,40 @@ def concatenare_elemente_palindrom(list1,list2):
         b = list2[i]
         x = 0
         while b != 0:
-            b = b \\ 10
-            x ++
+            b = b // 10
+            x = x + 1
         for j in range(1,x,1):
             a = a * 10
         a = a + list2[i]
         list_con.append(a)
-for i in range(0,len(list_con),1)
-    x = list_con[i]
-    
-
-
-
-def print_lista(list)
+    for i in range(0,len(list_con),1):
+        oglindit = oglindit_element(list_con[i])
+        if oglindit == list_con[i]:
+            palindrom.append(list_con[i]
+    )
+    return palindrom
+def oglindit_element(x):
+    while x != 0:
+        c = x % 10
+        oglindit = oglindit * 10 + c
+        x = x // 10
+    return oglindit
+def inlocuire_elemente_cu_oglinditul(list,list3):
+    for i in range(0,len(list),1):
+        k = 1
+        for j in range(0,len(list3),1):
+            if list[i] % list3[j] != 0:
+                k = 0
+        if k == 1:
+            list[i] = oglindit_element(list[i])
+    return list
+def print_lista(list):
     for i in range(0, len(list), 1):
         print(list[i])
 def main():
     list1 = []
     list2 = []
+    list3 =[]
     while True:
         print('1. Citirea a două mulțimi de numere întregi de la tastatura sub forma a două liste.')
         print('2. Afișați dacă cele două liste au același număr de elemente pare.')
@@ -55,9 +71,10 @@ def main():
         print('4. Afișați toate palindroamele obținute prin concatenarea elementelor de pe aceleași poziții în cele două liste.')
         print('5. Oglinditul elementelor')
         print('6. Exit')
-          opt = input('Alegeti optiunea: ')
+        opt = input('Alegeti optiunea: ')
         if opt == '1':
-            read(list1, list2)
+            read(list1)
+            read(list2)
         elif opt == '2':
             if numara_elementele_pare(list1) == numara_elementele_pare(list2):
                 print('Listele au acelasi numar de elemente pare')
@@ -66,9 +83,13 @@ def main():
         elif opt == '3':
             print_lista(intersectia_listelor(list1,list2))
         elif opt == '4':
-            ...
+            print_lista(concatenare_elemente_palindrom(list1,list2))
         elif opt == '5':
-            ...
-        elif opt =='6':
+            read(list3)
+            print(inlocuire_elemente_cu_oglinditul(list1,list3))
+            print(inlocuire_elemente_cu_oglinditul(list2,list3))
+        elif opt == '6':
             break
-        else print('Optiune invalida. Incercati din nou.')
+        else:
+            print('Optiune invalida. Incercati din nou.')
+main()
